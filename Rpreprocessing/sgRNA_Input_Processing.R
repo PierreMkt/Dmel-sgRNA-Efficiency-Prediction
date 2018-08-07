213780f821d815ccbe80f93a07279b32eaec9dad
PierreMkt <merckaert.pierre@gmail.com>
2018-08-07 18:02:42 -0400

    comments

--- Rpreprocessing/sgRNA_Input_Processing.R
+++ Rpreprocessing/sgRNA_Input_Processing.R
@@ -5,9 +5,17 @@
 # Date: 06/2018
 # 
 # Description: Extract the features required for efficiency prediction from 30mer sgRNA sequences
-# Input: 30mer sgRNA or csv file listing multiple sgRNA in the first column
+# Input: 20/23/30mer sgRNA or csv file listing multiple sgRNA in the first column
 # Output: csv file of all the features extracted from the input sequences
 # The format of the output csv is (number of columns) : 30mer(1), 23mer(1), GC%(1), Tm(4), indep Order1(4), indep Order2(16), dep Order1(120), dep Order2(464), NGGN(16)
+
+# Usage :
+# To input a unique sgRNA :
+# Rscript sgRNA_Input_Processing.R [20/23/30mer sgRNA] 
+#
+# To input a csv file of several sgRNAs :
+# Rscript sgRNA_Input_Processing.R [.csv file] 
+
 
 #install required packages and import functions
 if(!require(stringr)) install.packages("stringr",repos = "http://cran.us.r-project.org")
